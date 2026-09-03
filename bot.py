151722223d085bc6a5273688d3e7e17158361300
kanijfatema9833-spec <kanij.fatema9833@gmail.com>
2026-09-03 17:27:37 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,24 @@
 import os
+import threading
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = threading.Thread(target=run)
+    t.start()
+
+keep_alive()
+
+import os
 import datetime
 import discord
 from discord import app_commands
